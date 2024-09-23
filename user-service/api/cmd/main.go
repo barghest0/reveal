71b917415e58f1d82fcfbc3ad0caef77e824a032
barghest <a.bolshov@microline.ru>
2024-09-23 18:39:57 +0300

    feature(api): added cors

--- user-service/api/cmd/main.go
+++ user-service/api/cmd/main.go
@@ -9,6 +9,21 @@ import (
 	"net/http"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")                   // Разрешить доступ с любых доменов
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS") // Разрешенные методы
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")       // Разрешенные заголовки
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	app_config := config.LoadConfig()
 	db_config := config.LoadDBConfig()
@@ -23,6 +38,6 @@ func main() {
 	router := routes.InitRoutes(userService)
 
 	log.Printf("Server starting on port %s", app_config.ServerHost+":"+app_config.Port)
-	http.ListenAndServe(":"+app_config.Port, router)
+	http.ListenAndServe(":"+app_config.Port, corsMiddleware(router))
 
 }
