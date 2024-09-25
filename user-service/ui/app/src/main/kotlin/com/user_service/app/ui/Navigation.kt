7a93402c73d12a85ae3edd0896d14ba9e6947ea2
barghest <a.bolshov@microline.ru>
2024-09-25 19:48:19 +0300

    feature(ui): implement redirect to login after success registration

--- user-service/ui/app/src/main/kotlin/com/user_service/app/ui/Navigation.kt
+++ user-service/ui/app/src/main/kotlin/com/user_service/app/ui/Navigation.kt
@@ -4,12 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.user_service.pages.login.ui.LoginScreen
 import com.user_service.pages.registration.ui.RegistrationScreen
 
 @Composable
 fun AppNavigation(navController: NavHostController) {
   NavHost(navController = navController, startDestination = "registration") {
-    composable("registration") { RegistrationScreen() }
-    // Добавьте другие экраны здесь
+    composable("registration") { RegistrationScreen(navController) }
+    composable("login") { LoginScreen() }
   }
 }
--- user-service/ui/app/src/main/kotlin/com/user_service/pages/registration/ui/RegistrationScreen.kt
+++ user-service/ui/app/src/main/kotlin/com/user_service/pages/registration/ui/RegistrationScreen.kt
@@ -1,15 +1,17 @@
 package com.user_service.pages.registration.ui
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import com.user_service.entities.user.model.repository.UserRepository
 import com.user_service.features.registration.domain.RegistrationUseCase
 import com.user_service.features.registration.presentation.RegistrationViewModel
@@ -21,29 +23,35 @@ import io.ktor.serialization.kotlinx.json.json
 
 @Composable
 fun RegistrationScreen(
+    navController: NavHostController,
     viewModel: RegistrationViewModel = remember {
       RegistrationViewModel(
           RegistrationUseCase(
-              UserRepository(
-                  HttpClient(CIO) { install(ContentNegotiation) { json() } })))
+              UserRepository(HttpClient(CIO) { install(ContentNegotiation) { json() } })))
     }
 ) {
   val state by viewModel.uiState
 
-  Column(
-      modifier = Modifier.fillMaxSize(),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center) {
-        RegistrationForm(
-            name = state.name,
-            email = state.email,
-            password = state.password,
-            updateField = { field, value -> viewModel.updateField(field, value) },
-            onRegisterClicked = { name, email, password ->
-              viewModel.register(name, email, password)
-            })
+  Box(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier.align(Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+          RegistrationForm(
+              name = state.name,
+              email = state.email,
+              password = state.password,
+              updateField = { field, value -> viewModel.updateField(field, value) },
+              onRegisterClicked = { name, email, password ->
+                viewModel.register(name, email, password)
+              })
 
-        // Отображение сообщений об ошибках
-        state.error?.let { Text(text = it, color = MaterialTheme.colorScheme.error) }
-      }
+          if (state.success) {
+            LaunchedEffect(Unit) {
+              navController.navigate("login") { popUpTo("registration") { inclusive = true } }
+            }
+          }
+
+          state.error?.let { Text(text = it, color = MaterialTheme.colorScheme.error) }
+        }
+  }
 }
