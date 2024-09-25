bf814541c4cebcb835f4c3c6085e65cbde1974a1
barghest <a.bolshov@microline.ru>
2024-09-25 20:02:29 +0300

    feature(ui): created link to login

--- user-service/ui/app/src/main/kotlin/com/user_service/pages/registration/ui/RegistrationScreen.kt
+++ user-service/ui/app/src/main/kotlin/com/user_service/pages/registration/ui/RegistrationScreen.kt
@@ -3,6 +3,7 @@ package com.user_service.pages.registration.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +12,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.navigation.NavHostController
 import com.user_service.entities.user.model.repository.UserRepository
 import com.user_service.features.registration.domain.RegistrationUseCase
@@ -43,6 +49,27 @@ fun RegistrationScreen(
               updateField = { field, value -> viewModel.updateField(field, value) },
               onRegisterClicked = { name, email, password ->
                 viewModel.register(name, email, password)
+              })
+
+          val annotatedText = buildAnnotatedString {
+            withStyle(style = SpanStyle(color = Color.White)) { append("Уже есть аккаунт? ") }
+            pushStringAnnotation(tag = "login", annotation = "login")
+            withStyle(
+                style = SpanStyle(color = Color.White, textDecoration = TextDecoration.Underline)) {
+                  append("Войти")
+                }
+            pop()
+          }
+
+          ClickableText(
+              text = annotatedText,
+              onClick = { offset ->
+                annotatedText
+                    .getStringAnnotations(tag = "login", start = offset, end = offset)
+                    .firstOrNull()
+                    ?.let {
+                      navController.navigate("login") // Переход на экран логина
+                    }
               })
 
           if (state.success) {
