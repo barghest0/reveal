f477c646eb0b057ae88dd64cb5580355a78b9876
barghest <a.bolshov@microline.ru>
2024-10-05 16:50:24 +0300

    feature(api): created products service api

---
+++ product-service/api/internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import "os"
+
+type Config struct {
+	Port       string
+	ServerHost string
+}
+
+type DBConfig struct {
+	User     string
+	Password string
+	Name     string
+	Host     string
+	Port     string
+}
+
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
+
+func LoadConfig() Config {
+	var AppConfig Config
+
+	AppConfig.Port = getEnv("PORT", "8080")
+	AppConfig.ServerHost = getEnv("SERVER_HOST", "localhost")
+
+	return AppConfig
+}
+
+func LoadDBConfig() DBConfig {
+	var DBConfig DBConfig
+
+	DBConfig.User = getEnv("POSTGRES_USER", "postgres")
+	DBConfig.Password = getEnv("POSTGRES_PASSWORD", "barghest")
+	DBConfig.Name = getEnv("POSTGRES_DB", "users")
+	DBConfig.Host = getEnv("DB_HOST", "localhost")
+	DBConfig.Port = getEnv("DB_PORT", "5432")
+
+	return DBConfig
+}
---
+++ product-service/api/internal/repository/repository.go
@@ -0,0 +1,63 @@
+package repository
+
+import (
+	"gorm.io/gorm"
+	"product-service/internal/model"
+	"time"
+)
+
+type ProductRepository interface {
+	Create(product *model.Product) error
+	GetByID(id uint) (*model.Product, error)
+	Update(product *model.Product) error
+	Delete(id uint) error
+	Purchase(id uint, buyerID uint) error
+}
+
+type productRepository struct {
+	db *gorm.DB
+}
+
+func CreateProductRepository(db *gorm.DB) ProductRepository {
+	return &productRepository{db}
+}
+
+func (r *productRepository) Create(product *model.Product) error {
+	return r.db.Create(product).Error
+}
+
+func (r *productRepository) GetByID(id uint) (*model.Product, error) {
+	var product model.Product
+	if err := r.db.First(&product, id).Error; err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (r *productRepository) Update(product *model.Product) error {
+	return r.db.Save(product).Error
+}
+
+func (r *productRepository) Delete(id uint) error {
+	return r.db.Delete(&model.Product{}, id).Error
+}
+
+func (r *productRepository) Purchase(id uint, buyerID uint) error {
+	var product model.Product
+
+	if err := r.db.First(&product, id).Error; err != nil {
+		return err
+	}
+
+	if product.BuyerID != nil {
+		return gorm.ErrInvalidData
+	}
+
+	buyer := buyerID
+	soldAt := time.Now()
+
+	return r.db.Model(&product).Updates(map[string]interface{}{
+		"buyer_id": buyer,
+		"sold_at":  soldAt,
+	}).Error
+}
