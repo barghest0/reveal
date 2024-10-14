ced15504eaaa4ee5ee8ebbd26867e71927e53ff2
lionlonn <lionlonn01@inbox.ru>
2024-10-14 15:22:32 +0300

    refactor(api): moved cart logic to cart-service

---
+++ api/cart-service/internal/model/cart.go
@@ -0,0 +1,18 @@
+package model
+
+import "time"
+
+type Cart struct {
+	ID        int       `json:"id"`
+	UserId    int       `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"update_at"`
+}
+
+type CartItem struct {
+	ID        int     `json:"id"`
+	CartId    int     `json:"cart_id"`
+	ProductID int     `json:"product_id"`
+	Quantity  int     `json:"quantity"`
+	Price     float64 `json:"price"`
+}
