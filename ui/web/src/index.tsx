1478ecd5329eb88f0a1ce17345c8f8a0432214f9
lionlonn <lionlonn01@inbox.ru>
2024-10-28 13:19:21 +0300

    feature(web): refactor webpack config use ts

--- ui/web/src/index.tsx
+++ ui/web/src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { App } from "./app";
 
 const container = document.getElementById('root');
@@ -11,3 +11,4 @@ root.render(
         <App/>
     </React.StrictMode>
 );
+
