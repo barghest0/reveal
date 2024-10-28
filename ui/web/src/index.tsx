728f3ff1b71c9cc5047e33b736440a1eef788daa
lionlonn <lionlonn01@inbox.ru>
2024-10-28 12:38:22 +0300

    feature(web): refuctor folder in ui

--- ui/web/src/index.tsx
+++ ui/web/src/index.tsx
@@ -4,6 +4,10 @@ import { App } from "./app";
 
 const container = document.getElementById('root');
 
-const root = ReactDOM.createRoot(container);
+const root = ReactDOM.createRoot(container as HTMLDivElement);
 
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
