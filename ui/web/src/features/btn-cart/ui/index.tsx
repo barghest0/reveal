8ec8ebef75ccf4f1102f95ee70ec682114f94c83
lionlonn <lionlonn01@inbox.ru>
2024-10-31 10:35:32 +0300

    feature(web): btn adding product to cart

---
+++ ui/web/src/features/btn-cart/ui/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+interface Props {
+
+}
+
+export const AddProductToCart: React.FC<Props> = () => {
+    return (
+        <>
+            <Button variant="contained">Add to cart</Button>
+        </>
+    )
+}
--- ui/web/src/index.tsx
+++ ui/web/src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import { App } from "./app/app";
+import { Provider } from "react-redux";
+import { store } from "app/store";
 
 const container = document.getElementById('root');
 
@@ -8,7 +10,9 @@ const root = ReactDOM.createRoot(container as HTMLDivElement);
 
 root.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </React.StrictMode>
 );
 
