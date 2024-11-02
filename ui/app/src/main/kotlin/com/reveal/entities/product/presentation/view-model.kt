c51699c5f82cc227089c3307696970a8540adaca
barghest <a.bolshov@microline.ru>
2024-11-02 14:22:45 +0300

    feature(ui): show product details on product page

--- ui/app/src/main/kotlin/com/reveal/entities/product/presentation/view-model.kt
+++ ui/app/src/main/kotlin/com/reveal/entities/product/presentation/view-model.kt
@@ -1,6 +1,7 @@
 package entities.product
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
@@ -8,9 +9,13 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class ProductViewModel(private val repository: ProductRepository) : ViewModel() {
+
   private val _products = mutableStateListOf<Product>()
   val products: List<Product>
     get() = _products
+
+  var product = mutableStateOf<Product?>(null)
+    private set
 
   fun fetchProducts() {
     viewModelScope.launch {
@@ -21,6 +26,19 @@ class ProductViewModel(private val repository: ProductRepository) : ViewModel() 
             _products.clear()
             _products.addAll(fetchedProducts)
           }
+        } catch (e: Exception) {
+          println(e)
+        }
+      }
+    }
+  }
+
+  fun fetchProduct(id: String) {
+    viewModelScope.launch {
+      withContext(Dispatchers.IO) {
+        try {
+          val fetchedProduct = repository.getProduct(id)
+          product.value = fetchedProduct
         } catch (e: Exception) {
           println(e)
         }
