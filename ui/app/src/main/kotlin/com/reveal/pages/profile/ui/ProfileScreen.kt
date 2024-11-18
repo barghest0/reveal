6670ef2a7c8a444b21ac33eff8bc710fd74d9ed2
barghest <a.bolshov@microline.ru>
2024-11-18 20:01:40 +0300

    fix(ui): get user profile

--- ui/app/src/main/kotlin/com/reveal/pages/profile/ui/ProfileScreen.kt
+++ ui/app/src/main/kotlin/com/reveal/pages/profile/ui/ProfileScreen.kt
@@ -1,26 +1,31 @@
 package pages.profile
 
-import androidx.compose.foundation.text.ClickableText
+import android.content.Context
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import entities.user.UserRepository
 import features.profile.ProfileViewModel
 import features.profile.ProfileViewModelFactory
 import kotlinx.coroutines.*
+import shared.session.PreferencesManager
 import shared.ui.layout.ScreenLayout
 
 @Composable
 fun ProfileScreen(
         navController: NavController,
-        viewModel: ProfileViewModel = viewModel(factory = ProfileViewModelFactory(UserRepository()))
+        context: Context = LocalContext.current,
+        viewModel: ProfileViewModel =
+                viewModel(
+                        factory =
+                                ProfileViewModelFactory(
+                                        UserRepository(),
+                                        PreferencesManager(context)
+                                )
+                )
 ) {
 
   val profile by viewModel.profileState
@@ -28,28 +33,6 @@ fun ProfileScreen(
   ScreenLayout {
     if (profile != null) {
       Text(text = "Имя: ${profile?.name}")
-      // Отобразите другую информацию профиля
-    } else {
-      val annotatedText = buildAnnotatedString {
-        withStyle(style = SpanStyle(color = Color.White)) { append("Нет аккаунта? ") }
-        pushStringAnnotation(tag = "registration", annotation = "registration")
-        withStyle(
-                style = SpanStyle(color = Color.White, textDecoration = TextDecoration.Underline)
-        ) { append("Регистрация") }
-        pop()
-      }
-
-      ClickableText(
-              text = annotatedText,
-              onClick = { offset ->
-                annotatedText
-                        .getStringAnnotations(tag = "registration", start = offset, end = offset)
-                        .firstOrNull()
-                        ?.let {
-                          navController.navigate("registration") // Переход на экран регистрации
-                        }
-              }
-      )
     }
   }
 }
