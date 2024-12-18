c2faed9055e6349d1368355f98e8a785d1eb6756
barghest <a.bolshov@microline.ru>
2024-12-18 19:00:54 +0300

    feat(ui): integrate firebase google services to android app

--- ui/android/app/build.gradle.kts
+++ ui/android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
   kotlin("plugin.serialization") version "1.9.0"
+  id("com.google.gms.google-services")
 }
 
 android {
@@ -49,6 +50,8 @@ dependencies {
   implementation(libs.androidx.navigation.compose)
   implementation(libs.androidx.ui.tooling.preview)
   implementation(libs.androidx.material3)
+  implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+  implementation("com.google.firebase:firebase-analytics")
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
